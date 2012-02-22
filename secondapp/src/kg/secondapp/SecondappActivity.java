7324af2b89313898084a3d4eb14407815e14454e
Temirlan Nurdinov <diego_10a@mail.ru>
2012-02-23 02:34:44 +0600

    my first time commiting with eclipse

---
+++ secondapp/src/kg/secondapp/SecondappActivity.java
@@ -0,0 +1,13 @@
+package kg.secondapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondappActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ secondapp/src/kg/secondapp/Splash.java
@@ -0,0 +1,17 @@
+package kg.secondapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		
+	}
+	
+
+}
